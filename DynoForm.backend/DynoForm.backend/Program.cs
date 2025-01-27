8b230a5397ef52967791b640160bcd45572cd0de
Niloo Mont <niloo.mon@gmail.com>
2025-01-27 19:14:44 +0330

    config: set CORS

--- DynoForm.backend/DynoForm.backend/Program.cs
+++ DynoForm.backend/DynoForm.backend/Program.cs
@@ -11,6 +11,31 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DynoDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins");
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", policy =>
+    {
+        var environment = builder.Environment.EnvironmentName;
+
+        if (environment == "Development")
+        {
+            var devOrigins = allowedOrigins.GetSection("Development").Get<string[]>();
+            policy.WithOrigins(devOrigins)
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        }
+        else if (environment == "Production")
+        {
+            var prodOrigin = Environment.GetEnvironmentVariable("MY_SERVER_IP");
+            policy.WithOrigins(prodOrigin)
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        }
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +43,8 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowSpecificOrigins");
 
 app.UseAuthorization();
 
